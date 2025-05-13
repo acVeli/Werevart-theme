e39f3308448537277fd43336f4cad8e2babf1382
acVeli <anton.covu@gmail.com>
2025-05-13 15:05:37 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name='robots' content='max-image-preview:large' />
+	<title>WeRevArt : Améliorons le mieux-être en milieu médical &#8211; WeRevArt facilite la prise en charge psychologique des patients en rendant les espaces de santé plus apaisants et plus accueillants grâce à l&#039;art dynamique.</title>
+	<link rel="stylesheet" href="wp-content/themes/astra-child/assets/css/custom.css">
+</head>
+
+<body>
+	
+
---
+++ index.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+	<section class="section-1">
+		<div class="video-background">
+			<video autoplay muted loop playsinline>
+				<source src="/wordpress/wp-content/themes/astra-child/assets/videos/lp_brush_sante_salle_verte-5.mp4" type="video/mp4">
+				Votre navigateur ne supporte pas la vidéo.
+			</video>
+			<div class="container text-section-1">
+				<h1><strong>Améliorons le mieux-être</strong></h1>
+				<h2>en milieu médical</h1>
+
+				<h3><strong>WeRevArt</strong> facilite la prise en charge psychologique<bR>
+				des patients en rendant les espaces de santé*<br>
+				<span class="primary-span">plus apaisants et plus accueillants</span><br>
+				<span class="secondary-span">grâce à l’art dynamique</span></h3>
+
+				<div class="btn-container">
+					<div>
+						<a href="#">Je veux voir une démo</a>
+					</div>
+				</div>
+
+				<p>* Salles d’attente, de consultation, d’examen des hôpitaux, cliniques, centres de soins spécialisés et cabinets médicaux</p>
+			</div>
+		</div>
+	</section>
+
+	
+	<section class="section-2">
+		<div class="container">
+			<h2><span class="primary-span">Le mal-être du patient</span> est <br>
+			un enjeu majeur</h2>
+			<h3>Lors d’une consultation, d’un examen ou d’une intervention, <span class="primary-span">un patient stressé, anxieux ou hypersensible</span> peut engendrer des :</h3>
+			<div class="solution-benefits">
+				<ul>
+					<li>
+						<div><img src="/wordpress/wp-content/themes/astra-child/assets/img/icone_erreur_diag_plein.png" alt=""></div>
+						<h4>Erreurs de<br> diagnostic</h4>
+					</li>
+					<li>
+						<div><img src="/wordpress/wp-content/themes/astra-child/assets/img/icone_coeur_danger_plein.png" alt=""></div>
+						<h4>Risques de<br> complications</h4>
+					</li>
+					<li>
+						<div><img src="/wordpress/wp-content/themes/astra-child/assets/img/icone_conflit_plein-1.png" alt=""></div>
+						<h4>Tensions avec les<br> professionnels de santé</h4>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</section>
+
+	<section class="section-3">
+		<div class="video-background">
+			<video autoplay muted loop playsinline>
+				<source src="/wordpress/wp-content/themes/astra-child/assets/videos/lp_brush_sante_salle_blanche_reduit.mp4" type="video/mp4">
+				Votre navigateur ne supporte pas la vidéo.
+			</video>
+			<div class="container text-section-2">
+				<h2>WeRevArt agit sur<br>
+				<span class="secondary-span">le moral du patient</span><br>
+				pour le guider dans<br>
+				un <strong class="primary-span">état émotionnel positif</strong></h2>
+
+				<p class="p-desc">Là* où le besoin de mieux-être est primordial, nous disposons<br>
+				<span class="primary-span">des tableaux d’art animés d’œuvres relaxantes</span> pour créer une<br>
+				<span class="secondary-span">atmosphère bienveillante</span> qui diminue le stress, l’attente,<br>
+				la solitude, la douleur du patient, et lui apporte réconfort, détente et évasion</p>
+
+				<p class="p-ast">* Urgences, anesthésie, chirurgie, pédiatrie, cancerologie, dialise, maladies rares, réanimation, rééducation….</p>
+			</div>
+		</div>
+	</section>
+
+	<section class="section-4">
+			<h3>Le patient est <span class="span-strong">plus apaisé,</span><br>
+			<strong class="span-stronger">plus coopératif et plus réceptif,</strong><br>
+			ce qui <span class="span-strong">facilite sa prise en charge,</span><br> 
+			sa résilience et sa récupération</h3>
+	</section>
+
+	<section class="section-5">
+		<div class="video-background">
+			<video autoplay muted loop playsinline>
+				<source src="/wordpress/wp-content/themes/astra-child/assets/videos/lp_brush_sante_salle_reduit_4-1.mp4" type="video/mp4">
+				Votre navigateur ne supporte pas la vidéo.
+			</video>
+			<div class="container text-section-3">
+				<h2>L’IMMERSIF</h2>
+				<h3>Notre tableau d’art animé,<br>
+				autonome et personnalisé</h3>
+				<ul>
+					<li><span class="span-primary">Personnalisation</span> de l’expérience à chaque nature de soins</li>
+					<li><span class="span-primary">Effet immédiat</span> et optimisation d’impact continu</li>
+					<li><span class="span-primary">Intégration</span> de votre communication préventive</li>
+					<li><span class="span-primary">Renouvellement</span> mensuel des œuvres</li>
+					<li><span class="span-primary">Parcours</span> émotionnels multi-espaces</li>
+				</ul>
+				<div class="btn-container">
+					<div>
+						<a href="#">Je veux en savoir plus</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="section-6">
+		<div class="container">
+			<h2><span class="span-primary">L’art dynamique,</span> au service des professionnels de la santé</h2>
+			<div class="first-row">
+				<div class="section-6-video">
+					<video src="\wordpress\wp-content\themes\astra-child\assets\videos\lp_cadre_yoga_bleue.mp4"></video>
+				</div>
+				<div class="section-6-text">
+					<h3>Les bienfaits de l’art</h3>
+					<p>Les neurosciences nous confirment les bienfaits de l’art sur notre <span class="span-primary">santé physique</span> et <span class="span-primary">mentale</span>. L’art nous nourrit, nous inspire, nous transporte, nous rassemble, nous soigne.</p>
+					<h3>L’art, une invitation au voyage</h4>
+					<p>L’art dynamique nous invite ainsi au <span class="span-primary">voyage intérieur</span> et renforce nos capacités cognitives <span class="span-primary">en stimulant notre attention, notre réflexion, notre imagination.</span> Cette immersion sensorielle d’introspection favorise un état de pleine conscience qui réduit nos pensées négatives. Il devient alors un catalyseur émotionnel en nous ancrant dans l’instant présent, nous faisant oublier nos peines et nos peurs.</p>
+				</div>
+			</div>
+			<div class="second-row">
+				<div class="section-6-text">
+					<h3>Les zones actives du mieux-être</h3>
+					<p>L’art, lorsqu’il est en mouvement, stimule plus activement notre cerveau limbique, responsable de nos émotions. En visualisant des œuvres apaisantes, nous générons de nouvelles connexions neuronales qui libèrent des neurotransmetteurs tels que <span class="span-primary">l’ocytocine</span>, la <span class="span-primary">dopamine</span>, la <span class="span-primary">sérotonine</span> et <span class="span-primary">l’endorphine</span>, et améliorent notre mieux-être global.</p>
+					<h3>L’art et ses possibilités infinies</h4>
+					<p>L’art dynamique est vivant. Il s’adapte à chaque <span class="span-primary">service de santé</span>, à chaque <span class="span-primary">pathologie</span>, à chaque <span class="span-primary">public</span>. Ses formes, ses couleurs, ses symboles sont <span class="span-primary">adaptables à volonté</span> pour répondre aux besoins et aux objectifs de tous les professionnels de santé et de leurs patients.</p>	
+				</div>
+				<div class="section-6-video">
+					<video src="\wordpress\wp-content\themes\astra-child\assets\videos\lp_cadre_vortex_violet-1.mp4"></video>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="section-7">
+		<div class="container">
+			<h3>Ils nous font <span class="span-third">confiance</span></h3>
+			<ul>
+				<li><img src="\wordpress\wp-content\themes\astra-child\assets\img\logo_euratechnologies.png" alt=""></li>
+				<li><img src="\wordpress\wp-content\themes\astra-child\assets\img\logo_frenchtech.png" alt=""></li>
+				<li><img src="\wordpress\wp-content\themes\astra-child\assets\img\logo_edih_francais_gris.png" alt=""></li>
+				<li><img src="\wordpress\wp-content\themes\astra-child\assets\img\logo_iterra_rouge_1500x500px.png" alt=""></li>
+				<li><img src="\wordpress\wp-content\themes\astra-child\assets\img\logo_hauts_de_france.png" alt=""></li>
+				<li><img src="\wordpress\wp-content\themes\astra-child\assets\img\logo_bpifrance.png" alt=""></li>
+			</ul>
+		</div>
+	</section>
+	<div class="bottom-blue-splash">
+		<img src="\wordpress\wp-content\themes\astra-child\assets\img\bg_xxs_bleu_clair_down.webp" alt="">
+	</div>
+	<section class="section-8">
+		<div class="container">
+			<h3>Foire aux questions</h3>
+			<div class="faq-container">
+				<details>
+					<summary class="h4-primary">Quelle est la durée d’accompagnement ?</summary>
+					<p>La durée d’accompagnement est de <span class="span-primary">12 mois</span> renouvelable pour maintenir vos objectifs d’impact constants et croissants.</p>
+				</details>
+				<details>
+					<summary>Les écrans sont-ils compris dans votre offre ?</summary>
+					<p>Pour une meilleure expérience, nous préférons <span class="span-primary">inclure les écrans à notre offre</span>, gage de qualité et d’optimisation de l’impact. Toutefois, notre dispositif est également compatible avec vos équipements et s’intègre à votre système de diffusion.</p>
+				</details>
+				<details>
+					<summary class="h4-primary">Est-ce que je peux tester votre solution ?</summary>
+					<p>Oui. Après avoir identifié vos objectifs et vos besoins, nous pouvons <span class="span-primary">programmer des tests</span> préalables avec une <span class="span-primary">playlist expérimentale</span>.</p>
+				</details>
+				<details>
+					<summary>Quelle est votre valeur ajoutée par rapport à des œuvres traditionnelles ?</summary>
+					<p>Au-delà de l’impact plus important sur nos émotions, le <strong class="span-primary">streaming d’art</strong> permet de créer des <span class="span-primary">playlists personnalisées</span>, de les renouveler à volonté et d’y associer votre <span class="span-primary">communication interne</span>. C’est une solution sans effort qui est <span class="span-primary">plus économique</span> <span class="span-and">et</span> <span class="span-primary">plus rentable</span> que l’art traditionnel.</p>
+				</details>
+				<details>
+					<summary class="h4-primary">Comment diffusez-vous les œuvres dynamiques ?</summary>
+					<p>Nous utilisons soit notre propre API soit des solutions externes qui nous permet de <span class="span-primary">programmer</span> et de <span class="span-primary">gérer en temps réel</span> l’affichage des playlists sur les écrans.</p>
+				</details>
+				<details>
+					<summary>Comment je choisis les ambiances ?</summary>
+					<p>Selon vos <span class="span-primary">priorités</span>, votre <span class="span-primary">public</span> et <span class="span-primary">l’espace de diffusion</span>, nous sélectionnons des œuvres et créons des playlists que vous pourrez valider avant diffusion.</p>
+				</details>
+				<details>
+					<summary class="h4-primary">Quels sont les types d’œuvres proposées ?</summary>
+					<p>Notre choix aujourd’hui se porte sur les <span class="span-primary">œuvres en mouvement</span> car elles génèrent des émotions et des comportements mieux ciblés. Notre catalogue est <span class="span-primary">extrêmement varié</span> et nous permet de créer des univers singuliers en jouant sur les <span class="span-primary">formes</span>, les <span class="span-primary">couleurs</span>, les <span class="span-primary">thèmes</span>, les <span class="span-primary">mouvements</span>, les <span class="span-primary">perspectives</span>.</p>
+				</details>
+			</div>
+		</div>
+	</section>
+
+<?php get_footer(); ?>
